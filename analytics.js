890f33abb99881ac4a1c8fdc662e69caba27a742
mo <crispyyvfx@gmail.com>
2025-06-14 22:21:09 +0200

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const ANALYTICS_ENDPOINT_URL = 'http://localhost:3000/api/track-click';
+    const ANALYTICS_ENDPOINT_URL = 'nemzpacks.vercel.app';
 
     function sendScenepackClickToBackend(packName) {
         console.log(`[Frontend] Sending click for "${packName}" to the backend.`);
